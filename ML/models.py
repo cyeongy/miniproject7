8672b2032936f5f29b7aa03297514c52961b713a
cyeongy <cyeongy@gmail.com>
2022-11-22 15:51:55 +0900

    Rebase commit

--- ML/models.py
+++ ML/models.py
@@ -3,15 +3,6 @@ from django.db import models
 # Create your models here.
 class ML_Model(models.Model):
     title=models.CharField('TITLE', max_length=20)
-<<<<<<< HEAD
-    version=models.FloatField('VERSION', default=1.0)
-    is_selected=models.BooleanField('IS_SELECTED', default=False)
-    date_published = models.DateField('DATE_PUBLISHED', auto_now_add=True) 
-    file = models.FileField(upload_to='models/')
-    # 자동정렬할거면    
-    # class Meta:
-    #     ordering = ('')
-=======
     version=models.FloatField('VERSION')
     is_selected=models.BooleanField('IS_SELECTED')
     date_published = models.DateField('DATE_PUBLISHED', auto_now_add=True)
@@ -19,7 +10,6 @@ class ML_Model(models.Model):
     
     class Meta:
         ordering = ('id',)
->>>>>>> bf2cf9e (Update: ML code)
 
     def __str__(self):
         return self.title
--- ML/tests.py
+++ ML/tests.py
@@ -1,33 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.contrib.auth import get_user_model
 from .models import ML_Model
 from django.conf import settings
 from django.core.files import File
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+import mock
+import shutil
+import tempfile
 
 # Create your tests here.
+MEDIA_ROOT = tempfile.mkdtemp()
 
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class Test_ML_Model(TestCase):
 
     def setUp(self):
         my_model = ML_Model()
-        my_model.name = 'test_model'
-
-        my_model.file = SimpleUploadedFile(
-            "test_cnn_model.h5",
-            b"cnn_model_binary_code"  # note the b in front of the string [bytes]
-        )
-
+        my_model.title = 'test_model'
+        # my_model.version = 1.0
+        # my_model.is_selected = True
+        test_mock = mock.MagicMock(spec=File)
+        test_mock.name = 'test_model.h5'
+        my_model.file = test_mock
         my_model.save()
 
+    @classmethod
+    def tearDownClass(cls):
+        def tearDownClass(cls):
+            shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+            super().tearDownClass()
+
     def test_Create_model(self):
-        test = ML_Model.objects.get(name='test_model')
-        self.assertEqual(test.name, 'test_model')
-        print(test.created)
+        test = ML_Model.objects.get(title='test_model')
+        print(test.id)
+        print(test.date_published)
         print(test.file.path)
-<<<<<<< HEAD
         print(test.file)
-=======
-        print(test.file)
->>>>>>> bf2cf9e (Update: ML code)
