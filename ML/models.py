b7f6b999e72faca1168be1f8febda1d63cbc8acf
eiden-xcv <eiden524@gmail.com>
2022-11-22 15:45:53 +0900

    Update: ML model files

--- ML/models.py
+++ ML/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class ML_Model(models.Model):
     title=models.CharField('TITLE', max_length=20)
-    version=models.FloatField('VERSION')
-    is_selected=models.BooleanField('IS_SELECTED')
+    version=models.FloatField('VERSION', default=1.0)
+    is_selected=models.BooleanField('IS_SELECTED', default=False)
     date_published = models.DateField('DATE_PUBLISHED', auto_now_add=True) 
-    
+    file = models.FileField(upload_to='models/')
     # 자동정렬할거면    
     # class Meta:
     #     ordering = ('')
